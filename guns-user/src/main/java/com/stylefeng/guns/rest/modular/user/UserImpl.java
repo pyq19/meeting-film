88f4c88bbd6bbc3907406b3d9e6a724b707d855d
wrq <pyq225@gmail.com>
2018-11-15 02:40:31 +0800

    add 4-3 用户服务与网关模块互相调用

--- guns-user/src/main/java/com/stylefeng/guns/rest/modular/user/UserImpl.java
+++ guns-user/src/main/java/com/stylefeng/guns/rest/modular/user/UserImpl.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Component;
 public class UserImpl implements UserAPI {
     @Override
     public boolean login(String username, String password) {
-        return true;
+        System.out.println("this is user service " + username + ":" + password);
+        return false;
     }
 }
