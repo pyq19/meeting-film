78981129c9d48f61a20bb1d8062caa36c27b085c
wrq <pyq225@gmail.com>
2018-11-16 22:25:49 +0800

    add 5-2 新建影片模块

---
+++ guns-film/src/main/java/com/stylefeng/guns/rest/modular/user/UserServiceImpl.java
@@ -0,0 +1,132 @@
+package com.stylefeng.guns.rest.modular.user;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.stylefeng.guns.api.user.UserAPI;
+import com.stylefeng.guns.api.user.UserInfoModel;
+import com.stylefeng.guns.api.user.UserModel;
+import com.stylefeng.guns.core.util.MD5Util;
+import com.stylefeng.guns.rest.common.persistence.dao.MoocUserTMapper;
+import com.stylefeng.guns.rest.common.persistence.model.MoocUserT;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.sql.Date;
+
+@Component
+@Service(interfaceClass = UserAPI.class, loadbalance = "roundrobin")    // 配置 dubbo 负载均衡策略：轮询
+public class UserServiceImpl implements UserAPI {
+
+    @Autowired
+    private MoocUserTMapper moocUserTMapper;
+
+    @Override
+    public boolean register(UserModel userModel) {
+        // 将注册信息实体转换为数据实体
+        MoocUserT moocUserT = new MoocUserT();
+        moocUserT.setUserName(userModel.getUsername());
+        moocUserT.setEmail(userModel.getEmail());
+        moocUserT.setAddress(userModel.getAddress());
+        moocUserT.setUserPhone(userModel.getPhone());
+        // 创建时间、修改时间 current_timestamp
+        // md5混淆加密 + 盐值 -> shiro 加密
+        String md5Password = MD5Util.encrypt(userModel.getPassword());
+        moocUserT.setUserPwd(md5Password);
+        // 将数据实体存入数据库
+        Integer insert = moocUserTMapper.insert(moocUserT);
+        if (insert > 0) {   // 插入成功
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int login(String username, String password) {
+        // 根据登录账号获取用户信息
+        MoocUserT moocUserT = new MoocUserT();
+        moocUserT.setUserName(username);
+        MoocUserT result = moocUserTMapper.selectOne(moocUserT);
+        // 根据结果与传来的密码加密后做匹配
+        if (result != null && result.getUuid() > 0) {
+            String md5Pass = MD5Util.encrypt(password);
+            if (result.getUserPwd().equals(md5Pass)) {
+                return result.getUuid();
+            }
+        }
+        return 0;
+    }
+
+    // 检测用户名是否存在，存在则返回 true
+    @Override
+    public boolean checkUsername(String username) {
+        EntityWrapper<MoocUserT> entityWrapper = new EntityWrapper<>();
+        entityWrapper.eq("user_name", username);
+        Integer result = moocUserTMapper.selectCount(entityWrapper);
+        if (result != null && result > 0) {
+            return true; // 用户名存在
+        }
+        return false; // 用户名不存在
+    }
+
+    private UserInfoModel dataObjectToUserInfo(MoocUserT moocUserT) {
+        UserInfoModel userInfoModel = new UserInfoModel();
+        userInfoModel.setUuid(moocUserT.getUuid());
+        userInfoModel.setHeadAddress(moocUserT.getHeadUrl());
+        userInfoModel.setPhone(moocUserT.getUserPhone());
+        userInfoModel.setUpdateTime(moocUserT.getUpdateTime().getTime());
+        userInfoModel.setEmail(moocUserT.getEmail());
+        userInfoModel.setUsername(moocUserT.getUserName());
+        userInfoModel.setNickname(moocUserT.getNickName());
+        userInfoModel.setLifeState("" + moocUserT.getLifeState());
+        userInfoModel.setBirthday(moocUserT.getBirthday());
+        userInfoModel.setAddress(moocUserT.getAddress());
+        userInfoModel.setSex(moocUserT.getUserSex());
+        userInfoModel.setBeginTime(moocUserT.getBeginTime().getTime());
+        userInfoModel.setBiography(moocUserT.getBiography());
+        return userInfoModel;
+    }
+
+    @Override
+    public UserInfoModel getUserInfo(int uuid) {
+        // 根据主键查询用户信息 MoocUserT
+        MoocUserT moocUserT = moocUserTMapper.selectById(uuid);
+        // 将 moocUserT 转换为 UserInfoModel
+        UserInfoModel userInfoModel = dataObjectToUserInfo(moocUserT);
+        // 返回 UserInfoModel
+        return userInfoModel;
+    }
+
+    // 用 sql date
+    private Date timeToDate(long time) {
+        Date date = new Date(time);
+        return date;
+    }
+
+    @Override
+    public UserInfoModel updateUserInfo(UserInfoModel userInfoModel) {
+        // 将传入的信息转换为 MoocUserT
+        MoocUserT moocUserT = new MoocUserT();
+        moocUserT.setUuid(userInfoModel.getUuid());
+        moocUserT.setNickName(userInfoModel.getNickname());
+        moocUserT.setLifeState(Integer.parseInt(userInfoModel.getLifeState()));
+        moocUserT.setBirthday(userInfoModel.getBirthday());
+        moocUserT.setBiography(userInfoModel.getBiography());
+        moocUserT.setBeginTime(null);
+        moocUserT.setHeadUrl(userInfoModel.getHeadAddress());
+        moocUserT.setEmail(userInfoModel.getEmail());
+        moocUserT.setAddress(userInfoModel.getAddress());
+        moocUserT.setUserPhone(userInfoModel.getPhone());
+        moocUserT.setUserSex(userInfoModel.getSex());
+        moocUserT.setUpdateTime(null);
+
+        // 将数据保存
+        Integer isSuccess = moocUserTMapper.updateById(moocUserT);
+        if (isSuccess > 0) {
+            // 按照 Id 把用户信息取出
+            UserInfoModel result = getUserInfo(moocUserT.getUuid());
+            return result;
+        }
+        // 如果没成功 就把原来的参数传回去
+        return userInfoModel;
+    }
+}
