fa503f9636508464677a59b8e40041b60b29ea4a
wrq <pyq225@gmail.com>
2018-11-17 02:29:33 +0800

    add 5-9 首页实现 - 影片查询

--- guns-film/src/main/java/com/stylefeng/guns/rest/modular/film/service/DefaultFilmServiceImpl.java
+++ guns-film/src/main/java/com/stylefeng/guns/rest/modular/film/service/DefaultFilmServiceImpl.java
@@ -1,12 +1,17 @@
 package com.stylefeng.guns.rest.modular.film.service;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.baomidou.mybatisplus.plugins.Page;
 import com.stylefeng.guns.api.film.FilmServiceAPI;
 import com.stylefeng.guns.api.film.vo.BannerVO;
 import com.stylefeng.guns.api.film.vo.FilmInfo;
 import com.stylefeng.guns.api.film.vo.FilmVO;
+import com.stylefeng.guns.core.util.DateUtil;
 import com.stylefeng.guns.rest.common.persistence.dao.MoocBannerTMapper;
+import com.stylefeng.guns.rest.common.persistence.dao.MoocFilmTMapper;
 import com.stylefeng.guns.rest.common.persistence.model.MoocBannerT;
+import com.stylefeng.guns.rest.common.persistence.model.MoocFilmT;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -19,6 +24,9 @@ public class DefaultFilmServiceImpl implements FilmServiceAPI {
 
     @Autowired
     private MoocBannerTMapper moocBannerTMapper;
+
+    @Autowired
+    private MoocFilmTMapper moocFilmTMapper;
 
     @Override
     public List<BannerVO> getBanners() {
@@ -34,14 +42,68 @@ public class DefaultFilmServiceImpl implements FilmServiceAPI {
         return null;
     }
 
-    @Override
-    public FilmVO getHotFilms(boolean isLimit, int nums) {
-        return null;
+    private List<FilmInfo> getFilmInfos(List<MoocFilmT> moocFilms) {
+        List<FilmInfo> filmInfos = new ArrayList<>();
+        for (MoocFilmT moocFilmT : moocFilms) {
+            FilmInfo filmInfo = new FilmInfo();
+            filmInfo.setScore(moocFilmT.getFilmScore());
+            filmInfo.setImgAddress(moocFilmT.getImgAddress());
+            filmInfo.setFilmType(moocFilmT.getFilmType());
+            filmInfo.setFilmScore(moocFilmT.getFilmScore());
+            filmInfo.setFilmName(moocFilmT.getFilmName());
+            filmInfo.setFilmId(moocFilmT.getUuid() + "");
+            filmInfo.setExpectNum(moocFilmT.getFilmPresalenum());
+            filmInfo.setBoxNum(moocFilmT.getFilmBoxOffice());
+            filmInfo.setShowTime(DateUtil.getDay(moocFilmT.getFilmTime()));
+            filmInfos.add(filmInfo);
+        }
+        return filmInfos;
     }
 
+    // 正在热映
+    // isLimit 决定了到底是首页的 8 张，还是列表页
+    @Override
+    public FilmVO getHotFilms(boolean isLimit, int nums) {
+        FilmVO filmVO = new FilmVO();
+        List<FilmInfo> filmInfos = new ArrayList<>();
+        // 热映影片的限制条件
+        EntityWrapper<MoocFilmT> entityWrapper = new EntityWrapper<>();
+        entityWrapper.eq("film_status", "1"); // (1-正在热映，2-即将上映，3-经典影片
+        // 判断是否是首页需要的内容
+        if (isLimit) {
+            // 如果是，则限制条数、限制内容为热映影片
+            Page<MoocFilmT> page = new Page<>(1, nums);
+            List<MoocFilmT> moocFilms = moocFilmTMapper.selectPage(page, entityWrapper);
+            // 组织 filmInfos
+            filmInfos = getFilmInfos(moocFilms);
+            filmVO.setFilmInfo(filmInfos);
+        } else {
+            // 如果不是，则是列表页，同样需要限制内容为热映影片
+
+        }
+        return filmVO;
+    }
+
+    // 即将上映
     @Override
     public FilmVO getSoonFilms(boolean isLimit, int nums) {
-        return null;
+        FilmVO filmVO = new FilmVO();
+        List<FilmInfo> filmInfos = new ArrayList<>();
+        // 热映影片的限制条件
+        EntityWrapper<MoocFilmT> entityWrapper = new EntityWrapper<>();
+        entityWrapper.eq("film_status", "2"); // (1-正在热映，2-即将上映，3-经典影片
+        // 判断是否是首页需要的内容
+        if (isLimit) {
+            // 如果是，则限制条数、限制内容为热映影片
+            Page<MoocFilmT> page = new Page<>(1, nums);
+            List<MoocFilmT> moocFilms = moocFilmTMapper.selectPage(page, entityWrapper);
+            // 组织 filmInfos
+            filmInfos = getFilmInfos(moocFilms);
+            filmVO.setFilmInfo(filmInfos);
+        } else {
+            // 如果不是，则是列表页，同样需要限制内容为热映影片
+
+        }
     }
 
     @Override
