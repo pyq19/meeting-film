20bfc95044e46910731fba844a0bd2b45af6c4d9
wrq <pyq225@gmail.com>
2018-11-17 10:41:11 +0800

    5-20 影片条件列表实现-controller 表现层业务实现

--- guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/film/FilmController.java
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/film/FilmController.java
@@ -2,12 +2,19 @@ package com.stylefeng.guns.rest.modular.film;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.stylefeng.guns.api.film.FilmServiceAPI;
+import com.stylefeng.guns.api.film.vo.CatVO;
+import com.stylefeng.guns.api.film.vo.SourceVO;
+import com.stylefeng.guns.api.film.vo.YearVO;
+import com.stylefeng.guns.rest.modular.film.vo.FilmConditionVO;
 import com.stylefeng.guns.rest.modular.film.vo.FilmIndexVO;
 import com.stylefeng.guns.rest.modular.vo.ResponseVO;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/film/")
@@ -43,13 +50,100 @@ public class FilmController {
             @RequestParam(name = "catId", required = false, defaultValue = "99") String catId,
             @RequestParam(name = "sourceId", required = false, defaultValue = "99") String sourceId,
             @RequestParam(name = "yearId", required = false, defaultValue = "99") String yearId) {
+        FilmConditionVO filmConditionVO = new FilmConditionVO();
+
+        // 标识位
+        boolean flag = false;
         // 类型集合
-            // 判断集合是否存在 catId，如果存在，则将对应的实体变成 active 状态
+        List<CatVO> cats = filmServiceAPI.getCats();
+        List<CatVO> catResult = new ArrayList<>();
+        CatVO cat = null;
+        for (CatVO catVO : cats) {
+            // 判断集合是否存在catId，如果存在，则将对应的实体变成active状态
+            // 6
+            // 1,2,3,99,4,5 ->
+            /*
+                优化：【理论上】
+                    1、数据层查询按Id进行排序【有序集合 -> 有序数组】
+                    2、通过二分法查找
+             */
+            if (catVO.getCatId().equals("99")) {
+                cat = catVO;
+                continue;
+            }
+            if (catVO.getCatId().equals(catId)) {
+                flag = true;
+                catVO.setActive(true);
+            } else {
+                catVO.setActive(false);
+            }
+            catResult.add(catVO);
+        }
+        // 如果不存在，则默认将全部变为Active状态
+        if (!flag) {
+            cat.setActive(true);
+            catResult.add(cat);
+        } else {
+            cat.setActive(false);
+            catResult.add(cat);
+        }
 
         // 片源集合
+        flag = false;
+        List<SourceVO> sources = filmServiceAPI.getSources();
+        List<SourceVO> sourceResult = new ArrayList<>();
+        SourceVO sourceVO = null;
+        for (SourceVO source : sources) {
+            if (source.getSourceId().equals("99")) {
+                sourceVO = source;
+                continue;
+            }
+            if (source.getSourceId().equals(catId)) {
+                flag = true;
+                source.setActive(true);
+            } else {
+                source.setActive(false);
+            }
+            sourceResult.add(source);
+        }
+        // 如果不存在，则默认将全部变为Active状态
+        if (!flag) {
+            sourceVO.setActive(true);
+            sourceResult.add(sourceVO);
+        } else {
+            sourceVO.setActive(false);
+            sourceResult.add(sourceVO);
+        }
 
         // 年代集合
-
-        return null;
+        flag = false;
+        List<YearVO> years = filmServiceAPI.getYears();
+        List<YearVO> yearResult = new ArrayList<>();
+        YearVO yearVO = null;
+        for (YearVO year : years) {
+            if (year.getYearId().equals("99")) {
+                yearVO = year;
+                continue;
+            }
+            if (year.getYearId().equals(catId)) {
+                flag = true;
+                year.setActive(true);
+            } else {
+                year.setActive(false);
+            }
+            yearResult.add(year);
+        }
+        // 如果不存在，则默认将全部变为Active状态
+        if (!flag) {
+            yearVO.setActive(true);
+            yearResult.add(yearVO);
+        } else {
+            yearVO.setActive(false);
+            yearResult.add(yearVO);
+        }
+        filmConditionVO.setCatInfo(catResult);
+        filmConditionVO.setSourceInfo(sourceResult);
+        filmConditionVO.setYearInfo(yearResult);
+        return ResponseVO.success(filmConditionVO);
     }
 }
