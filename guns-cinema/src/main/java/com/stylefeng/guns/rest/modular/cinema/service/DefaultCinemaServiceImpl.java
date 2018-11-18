5b8d420813a13f793015d84f33dbd5acea652460
wrq <pyq225@gmail.com>
2018-11-19 00:04:16 +0800

    add 6-11 Cinema模块实现 - 查询条件列表实现

--- guns-cinema/src/main/java/com/stylefeng/guns/rest/modular/cinema/service/DefaultCinemaServiceImpl.java
+++ guns-cinema/src/main/java/com/stylefeng/guns/rest/modular/cinema/service/DefaultCinemaServiceImpl.java
@@ -6,7 +6,10 @@ import com.baomidou.mybatisplus.plugins.Page;
 import com.stylefeng.guns.api.cinema.CinemaServiceAPI;
 import com.stylefeng.guns.api.cinema.vo.*;
 import com.stylefeng.guns.rest.common.persistence.dao.*;
+import com.stylefeng.guns.rest.common.persistence.model.MoocAreaDictT;
+import com.stylefeng.guns.rest.common.persistence.model.MoocBrandDictT;
 import com.stylefeng.guns.rest.common.persistence.model.MoocCinemaT;
+import com.stylefeng.guns.rest.common.persistence.model.MoocHallDictT;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -52,7 +55,6 @@ public class DefaultCinemaServiceImpl implements CinemaServiceAPI {
         if (cinemaQueryVO.getHallType() != 99) {  // %#3#%
             entityWrapper.like("hall_ids", "%#+" + cinemaQueryVO.getHallType() + "+#%");
         }
-
         // 将数据实体转换为业务实体
         List<MoocCinemaT> moocCinemaTS = moocCinemaTMapper.selectPage(page, entityWrapper);
         for (MoocCinemaT moocCinemaT : moocCinemaTS) {
@@ -76,19 +78,100 @@ public class DefaultCinemaServiceImpl implements CinemaServiceAPI {
     // 2、根据条件获取品牌列表 [除了就 99 以外，其他的数字为 isActive(前端渲染选中时红圈) ]
     @Override
     public List<BrandVO> getBrands(int brandId) {
-        return null;
+        boolean flag = false;
+        List<BrandVO> brandVOS = new ArrayList<>();
+        // 判断brandId是否存在
+        MoocBrandDictT moocBrandDictT = moocBrandDictTMapper.selectById(brandId);
+        // 判断brandId 是否等于 99
+        if (brandId == 99 || moocBrandDictT == null || moocBrandDictT.getUuid() == null) {
+            flag = true;
+        }
+        // 查询所有列表
+        List<MoocBrandDictT> moocBrandDictTS = moocBrandDictTMapper.selectList(null);
+        // 判断flag如果为true，则将99置为isActive
+        for (MoocBrandDictT brand : moocBrandDictTS) {
+            BrandVO brandVO = new BrandVO();
+            brandVO.setBrandName(brand.getShowName());
+            brandVO.setBrandId(brand.getUuid() + "");
+            // 如果flag为true，则需要99，如为false，则匹配上的内容为active
+            if (flag) {
+                if (brand.getUuid() == 99) {
+                    brandVO.setActive(true);
+                }
+            } else {
+                if (brand.getUuid() == brandId) {
+                    brandVO.setActive(true);
+                }
+            }
+            brandVOS.add(brandVO);
+        }
+        return brandVOS;
     }
 
-    // 3、获取行政区域列表
+    //3、获取行政区域列表
     @Override
     public List<AreaVO> getAreas(int areaId) {
-        return null;
+        boolean flag = false;
+        List<AreaVO> areaVOS = new ArrayList<>();
+        // 判断brandId是否存在
+        MoocAreaDictT moocAreaDictT = moocAreaDictTMapper.selectById(areaId);
+        // 判断brandId 是否等于 99
+        if (areaId == 99 || moocAreaDictT == null || moocAreaDictT.getUuid() == null) {
+            flag = true;
+        }
+        // 查询所有列表
+        List<MoocAreaDictT> moocAreaDictTS = moocAreaDictTMapper.selectList(null);
+        // 判断flag如果为true，则将99置为isActive
+        for (MoocAreaDictT area : moocAreaDictTS) {
+            AreaVO areaVO = new AreaVO();
+            areaVO.setAreaName(area.getShowName());
+            areaVO.setAreaId(area.getUuid() + "");
+            // 如果flag为true，则需要99，如为false，则匹配上的内容为active
+            if (flag) {
+                if (area.getUuid() == 99) {
+                    areaVO.setActive(true);
+                }
+            } else {
+                if (area.getUuid() == areaId) {
+                    areaVO.setActive(true);
+                }
+            }
+            areaVOS.add(areaVO);
+        }
+        return areaVOS;
     }
 
-    // 4、获取影厅类型列表
+    //4、获取影厅类型列表
     @Override
     public List<HallTypeVO> getHallTypes(int hallType) {
-        return null;
+        boolean flag = false;
+        List<HallTypeVO> hallTypeVOS = new ArrayList<>();
+        // 判断brandId是否存在
+        MoocHallDictT moocHallDictT = moocHallDictTMapper.selectById(hallType);
+        // 判断brandId 是否等于 99
+        if (hallType == 99 || moocHallDictT == null || moocHallDictT.getUuid() == null) {
+            flag = true;
+        }
+        // 查询所有列表
+        List<MoocHallDictT> moocHallDictTS = moocHallDictTMapper.selectList(null);
+        // 判断flag如果为true，则将99置为isActive
+        for (MoocHallDictT hall : moocHallDictTS) {
+            HallTypeVO hallTypeVO = new HallTypeVO();
+            hallTypeVO.setHalltypeName(hall.getShowName());
+            hallTypeVO.setHalltypeId(hall.getUuid() + "");
+            // 如果flag为true，则需要99，如为false，则匹配上的内容为active
+            if (flag) {
+                if (hall.getUuid() == 99) {
+                    hallTypeVO.setActive(true);
+                }
+            } else {
+                if (hall.getUuid() == hallType) {
+                    hallTypeVO.setActive(true);
+                }
+            }
+            hallTypeVOS.add(hallTypeVO);
+        }
+        return hallTypeVOS;
     }
 
     // 5、根据影院编号，获取影院信息
