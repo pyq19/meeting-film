9c8c19c336c55b09725df893db6181fdd6379c06
wrq <pyq225@gmail.com>
2018-11-19 00:29:39 +0800

    add 6-15 Cinema模块实现 - 查询特定场次的影片信息

--- guns-cinema/src/main/java/com/stylefeng/guns/rest/common/persistence/dao/MoocFieldTMapper.java
+++ guns-cinema/src/main/java/com/stylefeng/guns/rest/common/persistence/dao/MoocFieldTMapper.java
@@ -22,4 +22,5 @@ public interface MoocFieldTMapper extends BaseMapper<MoocFieldT> {
 
     HallInfoVO getHallInfo(@Param("fieldId") int fieldId);
 
+    FilmInfoVO getFilmInfoById(@Param("fieldId") int fieldId);
 }
