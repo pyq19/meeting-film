605a4c0f9496187a041aacf792549911033fc34d
wrq <pyq225@gmail.com>
2018-11-18 20:47:11 +0800

    add 6-7

--- guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/cinema/CinemaController.java
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/cinema/CinemaController.java
@@ -18,23 +18,34 @@ public class CinemaController {
     // 查询影院列表
     @RequestMapping(value = "getCinemas")
     public ResponseVO getCinemas(CinemaQueryVO cinemaQueryVO) {
+        // 按照五个条件进行筛选
+
+        // 判断是否有满足条件的影院
+
+        // 如果出现异常，应该如何处理
 
         return null;
     }
 
     // 获取影院列表查询条件
+    // 根据条件获取 品牌列表、行政区域列表、影厅类型列表
     @RequestMapping(value = "getCondition")
     public ResponseVO getCondition(CinemaQueryVO cinemaQueryVO) {
         return null;
     }
 
     // 获取播放场次接口
+    // 1. 根据影院编号，获取影院信息；2. 获取所有电影的信息和对应的放映场次信息
     @RequestMapping(value = "getFields")
     public ResponseVO getFields(Integer cinemaId) {
         return null;
     }
 
     // 获取场次详细信息
+    // 1. 根据影院编号获取影院信息
+    // 2. 根据放映场次 id 获取放映信息
+    // 3. 根据放映场次查询播放的电影编号，再根据电影编号获取对应的电影信息
+    // 4. 根据放映场次 id 获取已售座位
     @RequestMapping(value = "getFieldInfo", method = RequestMethod.POST)
     public ResponseVO getFieldInfo(Integer cinemaId, Integer fieldId) {
         return null;
