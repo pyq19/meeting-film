a6be3be2bb1af00639f4592b59da31b5805aff3e
wrq <pyq225@gmail.com>
2018-11-19 06:27:46 +0800

    add 6-20 全局异常处理

--- guns-core/src/main/java/com/stylefeng/guns/core/base/tips/SuccessTip.java
+++ guns-core/src/main/java/com/stylefeng/guns/core/base/tips/SuccessTip.java
@@ -7,9 +7,9 @@ package com.stylefeng.guns.core.base.tips;
  * @date 2016年11月12日 下午5:05:22
  */
 public class SuccessTip extends Tip {
-	
-	public SuccessTip(){
-		super.code = 200;
-		super.message = "操作成功";
-	}
+
+    public SuccessTip() {
+        super.status = 200;
+        super.msg = "操作成功";
+    }
 }
