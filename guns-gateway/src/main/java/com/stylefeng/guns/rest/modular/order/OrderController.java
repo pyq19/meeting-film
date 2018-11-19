1d8ca123a4e069502142dce3687ada713e66e791
wrq <pyq225@gmail.com>
2018-11-20 07:15:49 +0800

    add 7-18 订单模块 - 购票API网关实现

--- guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/order/OrderController.java
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/order/OrderController.java
@@ -1,5 +1,9 @@
 package com.stylefeng.guns.rest.modular.order;
 
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.stylefeng.guns.api.order.OrderServiceAPI;
+import com.stylefeng.guns.api.order.vo.OrderVO;
+import com.stylefeng.guns.rest.common.CurrentUser;
 import com.stylefeng.guns.rest.modular.vo.ResponseVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,16 +16,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/order/")
 public class OrderController {
 
+    @Reference(interfaceClass = OrderServiceAPI.class, check = false)
+    private OrderServiceAPI orderServiceAPI;
+
     // 购票
     @RequestMapping(value = "buyTickets", method = RequestMethod.POST)
     public ResponseVO buyTickets(Integer fieldId, String soldSeats, String seatsName) {
-        // 验证售出的票是否为真
-
-        // 已经销售的座位里，有没有这些座位
-
-        // 创建订单信息
-
-        return null;
+        try {
+            // 验证售出的票是否为真
+            boolean isTrue = orderServiceAPI.isTrueSeats(fieldId + "", soldSeats);
+            // 已经销售的座位里，有没有这些座位
+            boolean isNotSold = orderServiceAPI.isNotSoldSeats(fieldId + "", soldSeats);
+            // 验证，上述两个内容有一个不为真，则不创建订单信息
+            if (isTrue && isNotSold) {
+                // 创建订单信息,注意获取登陆人
+                String userId = CurrentUser.getCurrentUserId();
+                OrderVO orderVO = orderServiceAPI.saveOrderInfo(fieldId, soldSeats, seatsName, Integer.parseInt(userId));
+                if (orderVO == null) {
+                    log.error("购票未成功");
+                    return ResponseVO.serviceFail("购票业务异常");
+                } else {
+                    return ResponseVO.success(orderVO);
+                }
+            } else {
+                return ResponseVO.serviceFail("订单中的座位编号有问题");
+            }
+        } catch (Exception e) {
+            log.error("购票业务异常: ", e);
+            return ResponseVO.serviceFail("购票业务异常");
+        }
     }
 
     // 获取订单信息
