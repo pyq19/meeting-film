c920af59f6a508b3d607f258ac9bfdbd60cdfe8d
wrq <pyq225@gmail.com>
2018-11-19 22:43:58 +0800

    add 7-5 订单模块接口分析

--- guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/order/OrderController.java
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/order/OrderController.java
@@ -11,16 +11,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/order/")
 public class OrderController {
+
+    // 购票
     @RequestMapping(value = "buyTickets", method = RequestMethod.POST)
     public ResponseVO buyTickets(Integer fieldId, String soldSeats, String seatsName) {
+        // 验证售出的票是否为真
 
+        // 已经销售的座位里，有没有这些座位
+
+        // 创建订单信息
+
+        return null;
     }
 
+    // 获取订单信息
     @RequestMapping(value = "getOrderInfo", method = RequestMethod.POST)
     public ResponseVO getOrderInfo(
             @RequestParam(name = "nowPage", required = false, defaultValue = "1") Integer nowPage,
             @RequestParam(name = "pageSize", required = false, defaultValue = "5") Integer pageSize
     ) {
+        // 获取当前登陆人的信息
 
+        // 使用当前登陆人获取已经购买的订单
+
+        return null;
     }
 }
