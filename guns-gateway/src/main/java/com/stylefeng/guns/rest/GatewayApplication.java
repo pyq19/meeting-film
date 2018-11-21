6fe5552b0525d9557b23cc249b5b05804d08bdc2
wrq <pyq225@gmail.com>
2018-11-22 05:16:59 +0800

    add 7-32 Hystrix 熔断器

--- guns-gateway/src/main/java/com/stylefeng/guns/rest/GatewayApplication.java
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/GatewayApplication.java
@@ -3,11 +3,18 @@ package com.stylefeng.guns.rest;
 import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication(scanBasePackages = {"com.stylefeng.guns"})
 @EnableAsync
 @EnableDubboConfiguration
+// 开启 Hystrix 熔断器
+@EnableHystrixDashboard
+@EnableCircuitBreaker
+@EnableHystrix
 public class GatewayApplication {
 
     public static void main(String[] args) {
--- guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/order/OrderController.java
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/order/OrderController.java
@@ -2,6 +2,8 @@ package com.stylefeng.guns.rest.modular.order;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.baomidou.mybatisplus.plugins.Page;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.stylefeng.guns.api.order.OrderServiceAPI;
 import com.stylefeng.guns.api.order.vo.OrderVO;
 import com.stylefeng.guns.core.util.TokenBucket;
@@ -26,7 +28,30 @@ public class OrderController {
     @Reference(interfaceClass = OrderServiceAPI.class, check = false, timeout = 8000)
     private OrderServiceAPI orderServiceAPI;
 
+    // 服务降级
+    public ResponseVO error(Integer fieldId, String soldSeats, String seatsName) {
+        return ResponseVO.serviceFail("抱歉，下单的人太多了，请稍后重试");
+    }
+
     // 购票
+    /*
+        信号量隔离
+        线程池隔离
+        线程切换
+     */
+    @HystrixCommand(fallbackMethod = "error", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.strategy", value = "THREAD"),
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "4000"),
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "10"),
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "50")},
+            threadPoolProperties = {
+                    @HystrixProperty(name = "coreSize", value = "1"),
+                    @HystrixProperty(name = "maxQueueSize", value = "10"),
+                    @HystrixProperty(name = "keepAliveTimeMinutes", value = "1000"),
+                    @HystrixProperty(name = "queueSizeRejectionThreshold", value = "8"),
+                    @HystrixProperty(name = "metrics.rollingStats.numBuckets", value = "12"),
+                    @HystrixProperty(name = "metrics.rollingStats.timeInMilliseconds", value = "1500")
+            })
     @RequestMapping(value = "buyTickets", method = RequestMethod.POST)
     public ResponseVO buyTickets(Integer fieldId, String soldSeats, String seatsName) {
         try {
@@ -39,6 +64,7 @@ public class OrderController {
                 // 验证，上述两个内容有一个不为真，则不创建订单信息
                 if (isTrue && isNotSold) {
                     // 创建订单信息,注意获取登陆人
+                    // TODO BUG: 因为 Hystrix 注入了一个线程池导致这个线程池失效
                     String userId = CurrentUser.getCurrentUserId();
                     if (userId == null || userId.trim().length() == 0) {
                         return ResponseVO.serviceFail("用户未登陆");
